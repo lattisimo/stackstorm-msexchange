57ce3137baf1626f92e4d7a6d950c03dd70525d9
Shawn Robinson <shawn.robinson@sagenet.com>
2022-12-15 14:52:36 -0600

    try and submit email body as text

--- actions/send_email_o365.py
+++ actions/send_email_o365.py
@@ -1,16 +1,17 @@
 from base.action import BaseO365Action
-import codecs
+# import codecs
 
 
 class SendEmailAction(BaseO365Action):
-    def run(self, subject, body, to_recipients, store):
+    def run(self, subject, body, to_recipients, store, body_type='text'):
         mail = None
         if self.account.is_authenticated:
             self.logger.debug(self.account.get_current_user())
             mail = self.account.new_message()
             mail.to.add(to_recipients)
             mail.subject = subject
-            mail.body = codecs.decode(body, "utf-8")
+            mail.body_type = body_type
+            mail.body = body  # codecs.decode(codecs.encode(body, "utf-8"))
             mail.send(save_to_sent_folder=store)
         else:
             self.logger.error("Not Authenticated, Check Config and/or generate a token")
